120e1e30f9cdadd32f8d6940735ab7e25cbd27fa
zhenyuWang <13641039885@163.com>
2023-01-17 16:37:37 +0800

    fix(vue-devtools): App端调试多网卡可能无法连接问题

--- packages/shell-hbuilderx/src/backend.ts
+++ packages/shell-hbuilderx/src/backend.ts
@@ -14,49 +14,51 @@ interface DevtoolsSocket {
   disconnect: () => void
 }
 
-const socket = target.__VUE_DEVTOOLS_SOCKET__ as DevtoolsSocket
+target.__VUE_DEVTOOLS_ON_SOCKET_READY__(() => {
+  const socket = target.__VUE_DEVTOOLS_SOCKET__ as DevtoolsSocket
 
-const connectedMessage = () => {
-  if (target.__VUE_DEVTOOLS_TOAST__) {
-    target.__VUE_DEVTOOLS_TOAST__('Remote Devtools Connected', 'normal')
+  const connectedMessage = () => {
+    if (target.__VUE_DEVTOOLS_TOAST__) {
+      target.__VUE_DEVTOOLS_TOAST__('Remote Devtools Connected', 'normal')
+    }
   }
-}
 
-const disconnectedMessage = () => {
-  if (target.__VUE_DEVTOOLS_TOAST__) {
-    target.__VUE_DEVTOOLS_TOAST__('Remote Devtools Disconnected', 'error')
+  const disconnectedMessage = () => {
+    if (target.__VUE_DEVTOOLS_TOAST__) {
+      target.__VUE_DEVTOOLS_TOAST__('Remote Devtools Disconnected', 'error')
+    }
   }
-}
 
-socket.on('connect', () => {
-  connectedMessage()
-  initBackend(bridge)
-  socket.emit('vue-devtools-init')
-})
+  socket.on('connect', () => {
+    connectedMessage()
+    initBackend(bridge)
+    socket.emit('vue-devtools-init')
+  })
 
-// Global disconnect handler. Fires in two cases:
-// - after calling above socket.disconnect()
-// - once devtools is closed (that's why we need socket.disconnect() here too, to prevent further polling)
-socket.on('disconnect', () => {
-  socket.disconnect()
-  disconnectedMessage()
-})
+  // Global disconnect handler. Fires in two cases:
+  // - after calling above socket.disconnect()
+  // - once devtools is closed (that's why we need socket.disconnect() here too, to prevent further polling)
+  socket.on('disconnect', () => {
+    socket.disconnect()
+    disconnectedMessage()
+  })
 
-// Disconnect socket once other client is connected
-socket.on('vue-devtools-disconnect-backend', () => {
-  socket.disconnect()
-})
+  // Disconnect socket once other client is connected
+  socket.on('vue-devtools-disconnect-backend', () => {
+    socket.disconnect()
+  })
 
-const bridge = new Bridge({
-  listen (fn) {
-    socket.on('vue-message', (data) => fn(data))
-  },
-  send (data) {
-    socket.emit('vue-message', data)
-  },
-})
+  const bridge = new Bridge({
+    listen (fn) {
+      socket.on('vue-message', (data) => fn(data))
+    },
+    send (data) {
+      socket.emit('vue-message', data)
+    },
+  })
 
-bridge.on('shutdown', () => {
-  socket.disconnect()
-  disconnectedMessage()
+  bridge.on('shutdown', () => {
+    socket.disconnect()
+    disconnectedMessage()
+  })
 })
